e1590db13fcc5a53a2e072215f22421d9e59a31a
Upender Singh Lakhwan <lakhwanus009@gmail.com>
2026-03-31 01:21:37 +0530

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
 import Prototyper from './prototypes/Prototyper.tsx'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+
+inject()
 
 const isPrototype = window.location.pathname.startsWith('/prototype')
 
